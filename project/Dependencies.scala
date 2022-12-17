a6a9667d2e66a555ed7e9c5a6dc62d64ad73968d
Iurii Malchenko <yurique@astraea.io>
2022-12-17 15:46:04 +0100

    logging config, client version bump

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest        = "org.scalatest" %% "scalatest"         % "3.2.11"
-  lazy val kubernetesClient = "com.goyeau"    %% "kubernetes-client" % "0.9.0-19-3b18465-SNAPSHOT"
+  lazy val kubernetesClient = "com.goyeau"    %% "kubernetes-client" % "0.9.0-20-ae993cc-SNAPSHOT"
   lazy val decline          = "com.monovore"  %% "decline-effect"    % "2.3.1"
   lazy val logback          = "ch.qos.logback" % "logback-classic"   % "1.2.11"
 }
